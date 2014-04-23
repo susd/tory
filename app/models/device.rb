a4aa18f9b5010544e619f8e4c16e178ecb414068
Eric Hayes <eric@erichayes.net>
2014-04-23 09:47:31 -0700

    Push task model and device improvement

--- app/models/device.rb
+++ app/models/device.rb
@@ -23,6 +23,8 @@
 #
 
 class Device < ActiveRecord::Base
+  include HardwareExtraction
+  
   mount_uploader :xmlfile, XmlUploader
   mount_uploader :htmlfile, HtmlUploader
   
@@ -35,7 +37,13 @@ class Device < ActiveRecord::Base
   validates :mac_address, presence: true, uniqueness: true
   validates :site_id, presence: true
   
-  before_create :extract_from_xml!
+  def active_task?
+    !!tasks.where(state: 'active').any?
+  end
+  
+  def product
+    super || ""
+  end
   
   concerning :MacAddressing do
     def mac_address=(str)
@@ -49,80 +57,6 @@ class Device < ActiveRecord::Base
     def pxe_mac
       "01-" << mac_address.scan(/\w{2}/).join('-').downcase
     end
-  end
-  
-  
-  
-  def active_task?
-    !!tasks.where(state: 'active').any?
-  end
-  
-  def load_xml
-    @doc ||= Nokogiri::XML.parse(self.xmlfile.file.read)
-  end
-  
-  def extract_cpu!
-    load_xml
-    self.cpu = @doc.css("node.processor").first.css("product").text
-  end
-  
-  def extract_speed!
-    load_xml
-    mhz = @doc.css("node.processor").first.css("size").first.text.to_i / 10**6
-    self.cpu_speed = mhz
-  end
-  
-  def extract_ram!
-    load_xml
-    # RAM seems to be the first memory node consistently
-    mem = @doc.css('node[id*=memory]').first
-    bytes = mem.css('node[id*=bank] size').inject(0){|sum, s| sum + s.text.to_i}
-    self.ram = bytes_to_giga(bytes)
-  end
-  
-  def extract_banks!
-    load_xml
-    mem = @doc.css('node[id*=memory]').first # RAM seems to be the first memory node consistently
-    mem.css('node[id*=bank]').each do |bank|
-      pos = bank.attr('id').gsub(/\D/, '')
-      bytes = bank.css('size').text.to_i
-      if bytes > 0
-        banks[pos] = { id: bank.attr('id'), size: bytes_to_giga(bytes) }
-      else
-        banks[pos] = 'empty'
-      end
-    end
-  end
-  
-  def extract_make!
-    load_xml
-    self.make = @doc.css("node.system > vendor").text
-  end
-  
-  def extract_product!
-    load_xml
-    self.product = @doc.css("node.system > product").text
-  end
-  
-  def extract_serial!
-    load_xml
-    self.serial = @doc.css("node.system > serial").text
-  end
-  
-  def extract_uuid!
-    load_xml
-    self.uuid = @doc.css('setting#uuid').attr('value').text
-  end
-  
-  def extract_from_xml!
-    extract_cpu!
-    extract_speed!
-    extract_ram!
-    extract_banks!
-    extract_make!
-    extract_product!
-    extract_serial!
-    extract_uuid!
   end
   
   def best_name
