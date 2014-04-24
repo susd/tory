c15894813de96d2c28e900c0c4c01dd83a644073
Eric Hayes <eric@erichayes.net>
2014-04-24 13:50:42 -0700

    Rough admin section

---
+++ app/controllers/admin/sites_controller.rb
@@ -0,0 +1,40 @@
+class Admin::SitesController < AdminController
+  before_action :load_site, except: [:index, :new, :create]
+  def index
+    @sites = Site.order(:code)
+  end
+  
+  def new
+    @site = Site.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      redirect_to admin_sites_path, notice: 'Site created.'
+    else
+      render action: 'new'
+    end
+  end
+  
+  def update
+    if @site.update(site_params)
+      redirect_to admin_sites_path, notice: 'Site updated.'
+    else
+      render action: 'edit'
+    end
+  end
+  
+  private
+  
+  def load_site
+    @site = Site.find(params[:id])
+  end
+  
+  def site_params
+    params.require(:site).permit(:name, :abbr, :pxe, :storage, :code)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
   resources :tasks, except: [:new, :edit] 
   get 'tasks/:mac/finish', to: 'tasks#finish', as: :finish_task
   
-  resources :sites
+  resources :sites, only: [:show]
+  
+  namespace :admin do
+    resources :sites
+    resources :users
+  end
+  
   
   root to: 'devices#index'
   
