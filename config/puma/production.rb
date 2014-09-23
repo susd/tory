843b995eccebd8f8adbcd22050928a485f4c580b
Eric Hayes <eric@erichayes.net>
2014-09-23 14:50:08 -0700

    Tweaked puma settings

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,6 +1,6 @@
 app = 'tory'
 bind "unix:///tmp/#{app}_puma.sock"
-pidfile "/srv/rails/#{app}/current/tmp/puma/pid"
-state_path "/srv/rails/#{app}/current/tmp/puma/state"
+pidfile "/srv/rails/#{app}/current/tmp/pids/puma"
+state_path "/srv/rails/#{app}/current/tmp/puma_state"
 environment 'production'
 activate_control_app
